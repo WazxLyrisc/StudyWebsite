1b1fc2ebde1e2ade8c3f89b9fff6afe8c1d7956a
A NGHIA <wazxlyrisc@gmail.com>
2025-05-01 18:22:14 +0700

    last update

--- Website/javascript/firebase-config.js
+++ Website/javascript/firebase-config.js
@@ -1,23 +1,23 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAIS4uSCsGSGwGtkUEiMXk3X91_n0GM5pY",
-    authDomain: "studywebsite-fbdc6.firebaseapp.com",
-    projectId: "studywebsite-fbdc6",
-    storageBucket: "studywebsite-fbdc6.firebasestorage.app",
-    messagingSenderId: "43654871525",
-    appId: "1:43654871525:web:50cd34217348d3f658970c",
-    measurementId: "G-2ZMFK1XHCH"
-};
-
 // const firebaseConfig = {
-//     apiKey: "AIzaSyC3ig0W8pObVMAev3vw0ZDfxlSrKt8-CJc",
-//     authDomain: "giangson-jsi24.firebaseapp.com",
-//     projectId: "giangson-jsi24",
-//     storageBucket: "giangson-jsi24.firebasestorage.app",
-//     messagingSenderId: "906840388172",
-//     appId: "1:906840388172:web:36a63cfe6e5f31e18a3ae9",
-//     measurementId: "G-BPBX07284Y"
+//     apiKey: "AIzaSyAIS4uSCsGSGwGtkUEiMXk3X91_n0GM5pY",
+//     authDomain: "studywebsite-fbdc6.firebaseapp.com",
+//     projectId: "studywebsite-fbdc6",
+//     storageBucket: "studywebsite-fbdc6.firebasestorage.app",
+//     messagingSenderId: "43654871525",
+//     appId: "1:43654871525:web:50cd34217348d3f658970c",
+//     measurementId: "G-2ZMFK1XHCH"
 // };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3ig0W8pObVMAev3vw0ZDfxlSrKt8-CJc",
+    authDomain: "giangson-jsi24.firebaseapp.com",
+    projectId: "giangson-jsi24",
+    storageBucket: "giangson-jsi24.firebasestorage.app",
+    messagingSenderId: "906840388172",
+    appId: "1:906840388172:web:36a63cfe6e5f31e18a3ae9",
+    measurementId: "G-BPBX07284Y"
+};
 
 firebase.initializeApp(firebaseConfig);
 
